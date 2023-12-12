3203bf98855ef36b21016e512d662c84933b7fb1
theshrish46 <theshrish46@gmail.com>
2023-12-12 23:28:37 +0530

    added starters

---
+++ backend/src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./../constats.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_URL}/${DB_NAME}`
+    );
+    console.log(
+      `MongoDB connected !! Host : `,
+      connectionInstance.connection.host
+    );
+  } catch (error) {
+    console.log("MONGODB CONNECTION ERROR : ", error);
+    process.exit(1);
+  }
+};
---
+++ backend/src/index.js
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+import {app} from './app.js'
+
+dotenv.config({
+  path: "./.env",
+});
