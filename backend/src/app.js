620b3a530e523f6f4acabedbb43b7837a88f3113
theshrish46 <theshrish46@gmail.com>
2023-12-13 23:32:13 +0530

    connected db

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 const app = express();
-
-
-
+app.use(express.json({ limit: "32kb" }));
+app.use(express.urlencoded({ extended: true, limit: "32kb" }));
+app.use(cors({ credentials: true }));
+app.use(cookieParser());
 
 export { app };
