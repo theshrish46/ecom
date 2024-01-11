51ae5f6b2cf1a91991b73fbc7f94c7810729b40d
theshrish46 <theshrish46@gmail.com>
2024-01-11 23:24:35 +0530

    initial commit

--- backend/src/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-import { ApiError } from "./ApiError.js"
-
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
-    }
-}
-export {asyncHandler}
